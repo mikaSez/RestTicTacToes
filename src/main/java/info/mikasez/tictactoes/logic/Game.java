05c7313b61237f159ce27abac2134b89b2b704d5
MikaSez <mis.strator@gmail.com>
2016-04-02 23:59:37 +0200

    Board access logic

--- src/main/java/info/mikasez/tictactoes/logic/Game.java
+++ src/main/java/info/mikasez/tictactoes/logic/Game.java
@@ -21,4 +21,30 @@ public class Game {
         return board;
     }
 
+
+    /**
+     * This method write in the board at the given position assuming it's a standard tictactoes board
+     * Count begins from 1
+     * Starts in the upper-left corner
+     * @throws IllegalArgumentException : if x or y are not between 1-3 inclusive
+     * */
+    public void playAt(int x, int y) {
+        if(x < 1 || y < 1){
+            throw new IllegalArgumentException("X and Y should be at least 1");
+        }
+        if(x >3 || y > 3){
+            throw new IllegalArgumentException("X and Y should be at most 3");
+        }
+        System.out.printf("Played at : [%d, %d]\n", x, y);
+        System.out.printf("Real board position : [%d]\n", (x-1)+((y-1) * 3));
+        board[(x-1)+((y-1) * 3)] = true;
+    }
+
+    /**
+     * returns the corresponding board position
+     * @throws IndexOutOfBoundsException : direct access to the board.
+     * */
+    public boolean getAt(int i) {
+        return board[i];
+    }
 }
--- src/test/java/info/mikasez/tictactoes/logic/GameTest.java
+++ src/test/java/info/mikasez/tictactoes/logic/GameTest.java
@@ -28,4 +28,39 @@ public class GameTest {
     public void boardIs33(){
         Assert.assertEquals(9, tested.getBoard().length);
     }
+
+    @Test
+    public void boardCanBeWritten(){
+        tested.playAt(2, 2);
+        tested.playAt(1, 1);
+        tested.playAt(3,3);
+
+        boolean state5 = tested.getAt(4);
+        boolean state1 = tested.getAt(0);
+        boolean state9 = tested.getAt(8);
+        Assert.assertTrue(state5);
+        Assert.assertTrue(state1);
+        Assert.assertTrue(state9);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void xShouldBeAtLeast1(){
+       tested.playAt(0,1);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void yShouldBeAtLeast1(){
+        tested.playAt(1,0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void xShouldBeAtMost3(){
+        tested.playAt(4, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void yShouldBeAtMost3(){
+        tested.playAt(1,4);
+    }
+
+
 }
